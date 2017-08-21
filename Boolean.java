472cfc3b0090a4742997ba60faf20ee199e00e1d
aditya7188 <adityamech320@gmail.com>
2017-08-21 21:53:00 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,12 @@
+//boolean datatype demo
+class Boolean
+{
+   public static void main(String[] args)
+   {
+     boolean b1,b2;
+	  b1=10>5;
+	  b2=10<5;
+	  System.out.println("b1="+b1+"\t b2="+b2); //b1=true b2=false
+   }
+
+}
---
+++ Character.java
@@ -0,0 +1,16 @@
+// character datatype demo
+class Character
+{
+   public static void main(String[] args)
+   {
+      char ch1, ch2;
+	  ch1='A';
+	  ch2=98;
+	  System.out.println("ch1="+ch1+"\t ch2="+ch2);
+	  ch1++;
+	  ch2--;
+	  System.out.println("ch1="+ch1+"\t ch2="+ch2);
+	  
+   
+   }
+}
