8115fae9d2354df57242a694317a705a3c1aa9cb
aditya7188 <adityamech320@gmail.com>
2017-08-21 21:15:36 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,12 @@
+//t declare variables
+class Variable
+{
+   public static void main(String[] args)
+   {
+       int a=10,b=24;
+	   System.out.println("a value="+a); // + is concatination
+	   System.out.println("b value="+b);
+   
+   }
+
+}
